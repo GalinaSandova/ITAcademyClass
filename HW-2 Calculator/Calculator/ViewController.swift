6819e2fd65751a80e8c5ad6bb3870e8f684a398d
Galina <galka.sandova@gmail.coma>
2021-10-28 22:56:00 +0300

    [HW-2] Add buttonTapped

--- HW-2 Calculator/Calculator/ViewController.swift
+++ HW-2 Calculator/Calculator/ViewController.swift
@@ -8,12 +8,28 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var resalt: UILabel!
+    var caunter = 0
+    {
+        didSet  {
+            print(caunter)
+            updateLabel()
+        }
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    @IBAction func buttonTapped(_ sender: Any) {
+        if let sender = sender as? UIButton {
+            caunter = sender.tag }
+    }
+    
+ 
+    func updateLabel() {
+        resalt.text = String(caunter)
+     
 
-
+}
 }
 
