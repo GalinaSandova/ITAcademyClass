7184bb7fbb16f6bcdcdc0d18e911c1b34e718d91
Galina <galka.sandova@gmail.coma>
2021-10-28 10:20:39 +0300

    CW - working with layout, add views and colouring them.

---
+++ ITAcademyTest/ITAcademyTest/ConstraintDEMOViewController.swift
@@ -0,0 +1,56 @@
+//
+//  ConstraintDEMOViewController.swift
+//  ITAcademyTest
+//
+//  Created by Galka on 26.10.2021.
+//
+
+import UIKit
+
+class ConstraintDEMOViewController: UIViewController {
+  //  override func loadView() {
+       
+  //  }
+//override func viewDidLoad() {
+    
+//
+//       super.viewDidLoad()
+//
+//        // Do any additional setup after loading the view.
+//   }
+//
+//   // override func viewWillAppear(_ animated: Bool) {
+//        <#code#>
+//  //  }
+//  //  override func viewWillLayoutSubviews() {
+//        <#code#>
+////    }
+// //   override func viewDidLayoutSubviews() {
+//        <#code#>
+////    }
+////    override func viewDidAppear(_ animated: Bool) {
+//        <#code#>
+////    }
+// //   override func viewWillDisappear(_ animated: Bool) {
+//        <#code#>
+// //   }
+// //   override func viewDidDisappear(_ animated: Bool) {
+//        <#code#>
+// //   }
+// //   deinit {
+//
+//  //  }
+//
+//
+//    // MARK: - Navigation
+//
+//    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    /*
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+     */
+    
+
+}
