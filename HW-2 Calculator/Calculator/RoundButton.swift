1235a4dd415ffe7695f591c3c4b7c75911ff7fc3
Galina <galka.sandova@gmail.coma>
2021-10-28 10:26:11 +0300

    [HW2] Add buttons, make them round

---
+++ HW-2 Calculator/Calculator/RoundButton.swift
@@ -0,0 +1,24 @@
+//
+//  RoundButton.swift
+//  Calculator
+//
+//  Created by Galka on 27.10.2021.
+//
+
+import UIKit
+
+@IBDesignable class RoundButton: UIButton
+{
+    // Вызывается, когда кнопка меняет свои размеры
+    override func layoutSubviews() {
+        super.layoutSubviews()
+
+        updateCornerRadius()
+    }
+
+    
+    func updateCornerRadius() {
+        let min = min(frame.size.height, frame.size.width)
+        layer.cornerRadius =  min / 2
+    }
+}
