fb24115eb4bfaa1270c110611237a36951062339
Galina <galka.sandova@gmail.coma>
2021-10-30 00:27:35 +0300

    [HW-2] Add Calc logic

--- HW-2 Calculator/Calculator/RoundButton.swift
+++ HW-2 Calculator/Calculator/RoundButton.swift
@@ -9,14 +9,15 @@ import UIKit
 
 @IBDesignable class RoundButton: UIButton
 {
+    @IBInspectable var symbol: String = ""
+    @IBInspectable var toDo: Bool = true
+    
     // Вызывается, когда кнопка меняет свои размеры
     override func layoutSubviews() {
         super.layoutSubviews()
-
         updateCornerRadius()
     }
 
-    
     func updateCornerRadius() {
         let min = min(frame.size.height, frame.size.width)
         layer.cornerRadius =  min / 2
--- HW-2 Calculator/Calculator/ViewController.swift
+++ HW-2 Calculator/Calculator/ViewController.swift
@@ -9,27 +9,54 @@ import UIKit
 
 class ViewController: UIViewController {
     @IBOutlet weak var resalt: UILabel!
-    var caunter = 0
+ 
+    var counter: String = "0"
     {
         didSet  {
-            print(caunter)
-            updateLabel()
+            print(counter)
+            resalt.text = counter.isEmpty ? "0" : counter
         }
     }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-    @IBAction func buttonTapped(_ sender: Any) {
-        if let sender = sender as? UIButton {
-            caunter = sender.tag }
-    }
     
- 
-    func updateLabel() {
-        resalt.text = String(caunter)
-     
-
-}
+    @IBAction func buttonTapped(_ sender: Any) {
+        if let sender = sender as? RoundButton {
+            switch sender.symbol {
+            case "1","2","3","4","5","6","7","8","9":
+                if counter == "0" { counter = "" }
+                counter += "\(sender.symbol)"
+            case "0":
+                if counter != "0" {
+                    counter += "\(sender.symbol)"
+                }
+            case ",":
+                if !counter.contains(",") {
+                    counter += "\(sender.symbol)"
+                }
+            case "ac":
+                counter = "0"
+            case "+-":
+                if counter == "0" { return }
+                if counter == "0," { return }
+                if counter.contains("-") {
+                    counter.remove(at: counter.startIndex)
+                } else {
+                    counter.insert("-", at: counter.startIndex)
+                }
+            case "=":
+                counter = ""
+                resalt.text = "I don't know"
+            case "+","-","/","*","%":
+                let priview = counter
+                counter = ""
+                resalt.text = priview
+            default: break
+            }
+        }
+    }
 }
 
